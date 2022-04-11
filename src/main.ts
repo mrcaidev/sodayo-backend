935c3e35a66a93918a2ec4645c6084b16bf92939
mrcaidev <mrcaidev@qq.com>
2022-04-11 19:32:04 +0800

    :construction: Change main config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: "http://localhost:3000",
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+    }),
+  );
+  await app.listen(3600);
 }
 bootstrap();
