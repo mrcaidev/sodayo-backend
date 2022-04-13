37b0724e6890899f7d98a8f1b88f5d6d41121398
mrcaidev <mrcaidev@qq.com>
2022-04-13 20:40:37 +0800

    :sparkles: Add index greeting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "app.controller";
+import { AppService } from "app.service";
 import { configuration } from "config/configuration";
 import { AuthModule } from "./auth/auth.module";
+import { OrdersModule } from "./orders/orders.module";
 import { UsersModule } from "./users/users.module";
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -32,5 +33,6 @@ import { OrdersModule } from './orders/orders.module';
     OrdersModule,
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
